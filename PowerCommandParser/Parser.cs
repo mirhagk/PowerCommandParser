6254ffacb284223599adad281e8772131f8b43be
Nathan Jervis <mirhagk@gmail.com>
2015-04-11 17:50:56 -0400

    Initial commit
    project supports enough to pass the first simple unit test

---
+++ PowerCommandParser/Parser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerCommandParser
+{
+    public class RequiredAttribute : Attribute { }
+    public class Parser
+    {
+        public static T ParseArguments<T>(string[] args, bool outputErrors = true) where T :class, new()
+        {
+            T result = new T();
+            HashSet<string> providedArguments = new HashSet<string>();
+            var properties = typeof(T).GetProperties();
+            int positionArg = 0;
+            for(int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("--"))
+                {
+                    var switchName = args[i].Substring(2);
+                    var property = properties.SingleOrDefault(p => p.Name.Equals(switchName, StringComparison.InvariantCultureIgnoreCase));
+                    if (property == null || !property.PropertyType.IsAssignableFrom(typeof(bool)))
+                    {
+                        if (outputErrors)
+                            Console.Error.WriteLine($"No switch named {switchName} was found in the application");
+                        return null;
+                    }
+                    property.SetValue(result, true);
+                }
+                else if (args[i].StartsWith("-"))
+                {
+                    if (i + 1 == args.Length)
+                    {
+                        if (outputErrors)
+                            Console.Error.WriteLine("No value specified for argument {0}", args[i].Substring(1));
+                        return null;
+                    }
+                    var paramName = args[i].Substring(1);
+                    var property = properties.SingleOrDefault(p => p.Name.Equals(paramName, StringComparison.InvariantCultureIgnoreCase));
+                    if (property == null)
+                    {
+                        if (outputErrors)
+                            Console.Error.WriteLine($"No argument named {paramName} was found in the application");
+                        return null;
+                    }
+                    property.SetValue(result, args[++i]);
+                }
+                else
+                {
+
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTests/ParserTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerCommandParser;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ParserTest
+    {
+        private class BasicUsageOptions
+        {
+            public string Name { get; set; }
+            public bool Activate { get; set; }
+        }
+        public string[] CmdLineEntryParser(string input)
+        {
+            var cmdLineRegex = new Regex(@"(?:[^""\s]\S*)|(?:""[^""]*"")");
+            List<string> pieces = new List<string>();
+            foreach(Match match in cmdLineRegex.Matches(input))
+            {
+                pieces.Add(match.Value);
+            }
+            return pieces.ToArray();
+        }
+        [TestMethod]
+        public void BasicUsage()
+        {
+            var options = Parser.ParseArguments<BasicUsageOptions>(CmdLineEntryParser("--activate -name mirhagk"));
+            Assert.AreEqual(true, options.Activate);
+            Assert.AreEqual("mirhagk", options.Name);
+        }
+    }
+}
