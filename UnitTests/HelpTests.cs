42ea52d2f77dc65f758a7ef2067db4241e5625ca
Nathan Jervis <nathanj@airongroup.ca>
2016-08-19 23:04:23 -0400

    Started adding unit tests for help switch

---
+++ UnitTests/HelpTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HelpTests
+    {
+        public class NormalNoHelp
+        {
+            public int Value { get; set; }
+            public string Name { get; set; }
+        }
+        public class ClassWithHelp
+        {
+            [PowerCommandParser.Description("A name to use")]
+            public string Name { get; set; }
+        }
+        [TestMethod]
+        public void HelpDetected()
+        {
+            var oldOut = Console.Out;
+            var stringWriter = new System.IO.StringWriter();
+            Console.SetOut(stringWriter);
+            var result = PowerCommandParser.Parser.ParseArguments<NormalNoHelp>(TestHelper.CmdLineEntryParser("--help"));
+            Console.Out.Flush();
+            Console.SetOut(oldOut);
+
+            Assert.IsNull(result);
+            Assert.IsFalse(stringWriter.ToString().Contains("No switch named "));
+        }
+    }
+}
