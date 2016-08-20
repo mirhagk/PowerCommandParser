defbbe9663ea430c8a48faaade360504e05a5b3a
Nathan Jervis <nathanj@airongroup.ca>
2016-08-20 07:46:11 -0400

    Allow Descriptons on classes, fix syntax
    Syntax should not wrap the type is optional [] because that is the value, which is always required once the name is present. It's the whole parameter that can be optional

--- PowerCommandParser/HelpText.cs
+++ PowerCommandParser/HelpText.cs
@@ -9,58 +9,58 @@ using System.Threading.Tasks;
 
 namespace PowerCommandParser
 {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = true)]
     public class DescriptionAttribute : Attribute
     {
         public string Text { get; set; }
         public string Summary { get; set; }
-		public DescriptionAttribute(){}
-		public DescriptionAttribute(string summary)
-		{
-			Summary = summary;
-		}
+        public DescriptionAttribute() { }
+        public DescriptionAttribute(string summary)
+        {
+            Summary = summary;
+        }
         public DescriptionAttribute(string summary, string text)
         {
-			Summary = summary;
+            Summary = summary;
             Text = text;
         }
     }
-	public class HelpText<T>
-	{
-		public string Name { get; set; }
-		private DescriptionAttribute Description => typeof(T).GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
+    public class HelpText<T>
+    {
+        public string Name { get; set; }
+        private DescriptionAttribute Description => typeof(T).GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
         public string ProgramName => typeof(T).Assembly.GetName().Name;
         public string Synopsis => Description?.Summary ?? ProgramName;
-		public string LongDescription => Description?.Text; 
-		private IEnumerable<PropertyInfo> OrderedProperties => typeof(T).GetProperties().OrderBy(p=>p.GetCustomAttributes().Min(a=>(a as PositionAttribute)?.Position) ?? int.MaxValue); 
-		public string GetSyntax()
-		{
-			var result = Name;
-			foreach (var parameter in OrderedProperties)
-			{
-				var positioned = parameter.GetCustomAttribute(typeof(PositionAttribute)) != null;
-				var required = parameter.GetCustomAttribute(typeof(RequiredAttribute)) != null;
-				var parameterType = parameter.PropertyType.ToString();
-				var parameterText = parameter.Name;
-				if (positioned)
-					parameterText = $"[{parameterText}]";
-					
-				parameterText+=$" [{parameterType}]";
-				if (!required)
-					parameterText = $"[{parameterText}]";
-					
-				result += " " + parameterText;
-			}
-			
-			return result;
-		}
-		
-		public string GetParameterHelp()
-		{
-			return string.Join("\n\n", OrderedProperties
-							.Select(p=>new {p,d = p.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute})
-							.Where(x=>x.d!=null)
-							.Select(x=>$"{x.p.Name}:\n {x.d.Summary}"));
-		}
-	}
+        public string LongDescription => Description?.Text;
+        private IEnumerable<PropertyInfo> OrderedProperties => typeof(T).GetProperties().OrderBy(p => p.GetCustomAttributes().Min(a => (a as PositionAttribute)?.Position) ?? int.MaxValue);
+        public string GetSyntax()
+        {
+            var result = Name;
+            foreach (var parameter in OrderedProperties)
+            {
+                var positioned = parameter.GetCustomAttribute(typeof(PositionAttribute)) != null;
+                var required = parameter.GetCustomAttribute(typeof(RequiredAttribute)) != null;
+                var parameterType = parameter.PropertyType.ToString();
+                var parameterText = parameter.Name;
+                if (positioned)
+                    parameterText = $"[{parameterText}]";
+
+                parameterText += $" {parameterType}";
+                if (!required)
+                    parameterText = $"[{parameterText}]";
+
+                result += " " + parameterText;
+            }
+
+            return result;
+        }
+
+        public string GetParameterHelp()
+        {
+            return string.Join("\n\n", OrderedProperties
+                            .Select(p => new { p, d = p.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute })
+                            .Where(x => x.d != null)
+                            .Select(x => $"{x.p.Name}:\n {x.d.Summary}"));
+        }
+    }
 }
